66e6bd30f20c1a69c58a89ac398e71ec63d7381e
ashikur <ashikur1602@gmail.com>
2024-09-05 18:28:50 +0600

    working on portfolio page

--- src/pages/clientSide/portfolioPage/PortfolioPage.jsx
+++ src/pages/clientSide/portfolioPage/PortfolioPage.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
 
 const PortfolioPage = () => {
     return (
-        <div>
-            Portfolio page.
-        </div>
+        <>
+            <Helmet>
+                <title>UST | Portfolio</title>
+            </Helmet>
+            <div className='w-10/12 mt-20 mx-auto'>
+                <p className='text-2xl font-bold text-center py-5 text-universe_primary'>PORTFOLIO</p>
+                <div className="flex  gap-5 ">
+
+                    <div className='shadow-2xl border rounded-lg p-5'>
+                        <img src="https://res.cloudinary.com/dnvmj9pvk/image/upload/v1725537856/Universe%20Soft%20Tech/portfolioPage/q2nnxeoele8zh1dhbmjn.png" alt="" />
+                        <p className='font-bold text-2xl my-4'>UNIVERSE IT INSTITUTE</p>
+                        <div className=' flex justify-end'>
+                            <Link target="_blank" to={'https://www.universeitinstitute.com/'}>
+                                <button className='bg-universe_primary font-bold text-white p-5 text-xl rounded-lg'>Live Link</button>
+                            </Link>
+                        </div>
+                    </div>
+
+                    <div className='shadow-2xl border rounded-lg p-5'>
+                        <img src="https://res.cloudinary.com/dnvmj9pvk/image/upload/v1725537856/Universe%20Soft%20Tech/portfolioPage/q2nnxeoele8zh1dhbmjn.png" alt="" />
+                        <p className='font-bold text-2xl my-4'>UNIVERSE IT INSTITUTE</p>
+                        <div className=' flex justify-end'>
+                            <Link target="_blank" to={'https://www.universeitinstitute.com/'}>
+                                <button className='bg-universe_primary font-bold text-white p-5 text-xl rounded-lg'>Live Link</button>
+                            </Link>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
     );
 };
 
