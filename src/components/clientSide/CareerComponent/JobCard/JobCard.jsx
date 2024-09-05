ada11e1061f77411810c239e4167bbf986762d90
Mobinul Islam Mahi <mobinulislammahi@gmail.com>
2024-09-05 13:23:56 +0600

    motion change

--- src/components/clientSide/CareerComponent/JobCard/JobCard.jsx
+++ src/components/clientSide/CareerComponent/JobCard/JobCard.jsx
@@ -1,9 +1,11 @@
+import { ImLocation2 } from "react-icons/im";
+import { CiBookmark } from "react-icons/ci";
+import { AiFillDollarCircle } from "react-icons/ai";
+import { IoMdClock } from "react-icons/io";
 
-import { FaLocationDot } from "react-icons/fa6";
-import { FaBusinessTime } from "react-icons/fa6";
-import { MdManageHistory } from "react-icons/md";
-import { FaGraduationCap } from "react-icons/fa6";
-import './JobCard.css'
+
+
+// import './JobCard.css'
 
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
@@ -12,40 +14,31 @@ import { Link } from "react-router-dom";
 const JobCard = ({job}) => {
     
   
-    const {id,job_title,job_group,company_logo,location,experience,education,application_deadline,} = job
+    const {id,job_title, location,company_logo,experience,employed_status,skill,min_salary} = job
     return (
-        <Link to={`/career/${id}`} className="border-2 bg-[#E9EFF9] border-[#70ABDD] p-5 rounded-xl overSkill">
-            <div className="flex justify-between items-center">
-                <div>
-                     <h2 className="text-2xl font-bold text-[#5B985E]">{job_title}</h2>
-                     <p className="font-medium text-xl">{job_group}</p>
-                </div>
-                <div>
-                     <img className="w-20" src={company_logo} alt="" />
-                </div>
-            </div>
-            <div className="flex items-center gap-2 opacity-95">
-                <FaLocationDot></FaLocationDot>
-                <h2> {location}</h2>
-            </div>
-            <div className="flex justify-between opacity-95 pt-5">
-                <div className="flex items-center gap-2">
-                    <FaBusinessTime></FaBusinessTime>
-                    <p> {experience} years</p>
-                </div>
-                <div  className="flex items-center gap-2">
-                    <MdManageHistory className="text-xl font-medium"></MdManageHistory>
-                    <p>{application_deadline}</p>
-                </div>
-            </div>
+        <Link to={`/career/${id}`} >
+           <div className="border-y border-gray-400 hover:bg-slate-200 duration-75 px-2 cursor-pointer py-4">
+                            <div className="flex items-center justify-between">
+                                <h1 className="text-text_blue text-3xl font-semibold">{job_title}</h1>
+                                <CiBookmark />
+                            </div>
+                            <div className="flex items-center text-sm gap-2 pt-2">
+                                <ImLocation2 className="text-text_blue"/>
+                                <p className="text-gray-900 ">{location}</p>
+                            </div>
+                            <div className="flex items-center text-sm gap-2 pt-2">
+                                <IoMdClock  className="text-text_blue"/>
+                                <p className="text-gray-900 ">{employed_status }</p>
+                            </div>
+                            <div className="flex items-center text-sm gap-2">
+                         
+                                <AiFillDollarCircle className="text-text_blue"/>
+                                <p className="text-gray-900 ">Salary: {min_salary}</p>
+                            </div>
+                            <p className="text-gray-900 py-3">{`${skill}`.substring(0, 40)}...</p>
+                            <div className="badge badge-ghost">13 days ago</div>
+                        </div>
 
-            <div className="opacity-95 flex   py-2 gap-2  ">
-               <div>
-               <FaGraduationCap></FaGraduationCap>
-               </div>
-                <p>{education}</p>
-
-            </div>
             
         </Link>
     );
