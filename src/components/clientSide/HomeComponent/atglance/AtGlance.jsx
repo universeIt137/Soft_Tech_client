03dbabda09c85c437be05fd3485444e6badb291e
Mobinul Islam Mahi <mobinulislammahi@gmail.com>
2024-09-05 17:32:19 +0600

    initial at a glance

---
+++ src/components/clientSide/HomeComponent/atglance/AtGlance.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { PiNotePencilBold } from "react-icons/pi";
+
+const AtGlance = () => {
+  return (
+    <div className="container mx-auto py-28 text-center">
+      <h1 className=" text-lg">Universe Soft Tech</h1>
+      <h1 className="font-bold text-text_primari text-5xl">At a Glance</h1>
+      
+      <div className="flex justify-center pt-7">
+        <div className="shadow-lg shadow-gray-600 py-10 px-4 text-center">
+          <PiNotePencilBold />
+          <p>klfjdlkfj</p>
+        </div>
+      </div>
+
+
+      
+            
+    </div>
+  )
+};
+
+export default AtGlance
