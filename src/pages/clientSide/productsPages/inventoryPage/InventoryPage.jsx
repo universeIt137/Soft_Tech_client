92f8658e983876d2bafe2b4d0b02a7b3eed3e05e
ashikur <ashikur1602@gmail.com>
2024-09-08 17:55:31 +0600

    working on inventory

--- src/pages/clientSide/productsPages/inventoryPage/InventoryPage.jsx
+++ src/pages/clientSide/productsPages/inventoryPage/InventoryPage.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 
+import InventoryBanner from './InventoryBanner';
+import InventoryKeypoints from './InventoryKeypoints';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import InventoryDescription from './inventoryDescription';
+
 const InventoryPage = () => {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            delay:200,
+        })
+    })
+
     return (
         <>
-            <div className=' min-h-screen'>
-                <div className=' mt-20 flex justify-center items-center min-h-screen'>
-                    <div className='text-white space-y-5 text-center w-1/2'>
-                        <p className='text-universe_secondary text-7xl font-bold'>UNIVERSE SOFT TECH</p>
-                        <p className='text-5xl'>Inventory Management</p>
-                        <button className='btn'>Request a demo</button>
-                    </div>
-                    <div className='w-1/2'>
-                        <img src="https://res.cloudinary.com/dnvmj9pvk/image/upload/v1725785599/Universe%20Soft%20Tech/Products/Inventory/qz7pa4c1cczostrkl68h.jpg" alt="" />
-                    </div>
-                </div>
-            </div>
+            {/* banner section  */}
+            <InventoryBanner></InventoryBanner>
+
+            {/* keypoints section  */}
+            <InventoryKeypoints></InventoryKeypoints>
+
+            {/* description section  */}
+            <InventoryDescription></InventoryDescription>
         </>
     );
 };
