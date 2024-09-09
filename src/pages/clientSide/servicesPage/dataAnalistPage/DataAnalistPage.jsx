bdcbf23def7e11dec6706bde499c21c5b2159653
ashikur <ashikur1602@gmail.com>
2024-09-09 17:40:35 +0600

    Data Analyist added

--- src/pages/clientSide/servicesPage/dataAnalistPage/DataAnalistPage.jsx
+++ src/pages/clientSide/servicesPage/dataAnalistPage/DataAnalistPage.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const DataAnalistPage = () => {
     return (
         <div className='mt-20'>
-            Data analyist page.
+            <section className="banner-section bg-universe_secondary">
+                <div className="grid grid-cols-2 justify-center items-center w-4/5 mx-auto">
+                    <div className="">
+                        <p className='text-7xl font-bold text-white'>
+                            <span> Transform </span><br />
+                            Your Data <br />
+                            <span>Actionable</span>
+                        </p>
+                    </div>
+                    <div className="">
+                        <img src="https://res.cloudinary.com/dnvmj9pvk/image/upload/v1725881685/Universe%20Soft%20Tech/Services/E-commerce/sag4dspbswve3jlzd61q.png" alt="" />
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
