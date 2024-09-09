08581433b9eadf56dc6e0564a4b0546e206f2652
ashikur <ashikur1602@gmail.com>
2024-09-09 14:47:00 +0600

    change in dashboard

--- src/pages/clientSide/productsPages/posPage/PosPage.jsx
+++ src/pages/clientSide/productsPages/posPage/PosPage.jsx
@@ -6,7 +6,17 @@ const PosPage = () => {
         <div className='mt-20'>
             <div>
                 <div className='w-full bg-universe_primary min-h-screen'>
-                   
+                    <div className="grid grid-cols-2 border border-black justify-center items-center">
+                        <div className="">
+                            <div>
+                                <p>Universe Soft Tech</p>
+                                <p>POS system</p>
+                            </div>
+                        </div>
+                        <div className="">
+                            <img src="https://res.cloudinary.com/dnvmj9pvk/image/upload/v1725865380/Universe%20Soft%20Tech/Products/POS/co6mqcmbtae0vi2mghei.png" alt="" />
+                        </div>
+                   </div>
                 </div>
             </div>
         </div>
