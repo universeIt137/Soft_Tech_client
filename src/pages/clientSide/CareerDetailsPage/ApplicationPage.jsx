54a4d60314187b25f643fcbfbab03f5323229c36
ashikur <ashikur1602@gmail.com>
2024-09-29 16:16:19 +0600

    company profile page updated

--- src/pages/clientSide/CareerDetailsPage/ApplicationPage.jsx
+++ src/pages/clientSide/CareerDetailsPage/ApplicationPage.jsx
@@ -9,6 +9,7 @@ const ApplicationPage = () => {
   const [phoneNumber, setPhoneNumber] = React.useState('');
   const [address, setAddress] = React.useState('');
   const [resume, setResume] = React.useState('');
+  const [image, setImage] = React.useState('');
 
   const handleSubmitValue = async (e) => {
     e.preventDefault();
@@ -17,6 +18,7 @@ const ApplicationPage = () => {
     formData.append('phoneNumber', phoneNumber);
     formData.append('address', address);
     formData.append('resume', resume);
+    console.log(formData);
 
     const result = await axiosPublic.post(`/applyJob`,formData,{
       headers: {
@@ -102,6 +104,21 @@ const ApplicationPage = () => {
                 required
               />
             </div>
+
+            {/* <div className='w-full' >
+             
+              <label className="block text-lg font-medium text-gray-700 mb-2">
+              Formal Picture
+              </label>
+              <input
+                type="file"
+                accept='image/*'
+                name="image"
+                className="w-full px-4 py-2 rounded-lg border-2 border-gray-300"
+                onChange={(e)=>{setImage(e.target.files[0])}}
+                required
+              />
+            </div> */}
             </div>
             
             <div className='my-8' >
