ef43dd9d15c27c0714c8540b3f301f00a257a13e
ishanrana363 <ishanrana363@gmail.com>
2024-09-29 14:18:49 +0600

    aplay job

--- src/api-request/admin-api/application-api.js
+++ src/api-request/admin-api/application-api.js
@@ -1,27 +1,21 @@
-import axios from 'axios';
-
-
 import { create } from "zustand";
 import useAxiosPublic from '../../hooks/useAxiosPublic';
 const axiosPublic = useAxiosPublic();
 
 const ApplicationStore = create((set) => ({
-
-    
-
-    applicationList: null,
+    applicationList: [],
     getApplicationRequest:  async() => {
     try{
-        const  res = await axiosPublic.get(`/getApplication`)
-        console.log(res.data)
+        const res = await axiosPublic.get(`/all-applications`)   
+        console.log(`res is ${res} `)     
         if(res.data.status = 'success'){
+            
             set({applicationList: res.data['data']})
-            return res.data
         }else{
             return false
         }
     }catch(e){
-        console.log(e)
+        return false;
     }
 }
 }))
