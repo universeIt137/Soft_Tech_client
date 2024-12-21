2d9b2f31a243c271faa004ed2121007891fdfc95
ashikur <ashikur1602@gmail.com>
2024-12-21 14:19:35 +0600

    some changes

--- src/pages/adminSide/dashboard/DashboardLandingPage.jsx
+++ src/pages/adminSide/dashboard/DashboardLandingPage.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Profile = () => {
     return (
-        <div>
-            This is your profile
+        <div className='min-h-screen flex items-center'>
+            <p className="text-8xl font-bold text-center">Welcome to <br /> <span className="text-blue-500">Universe Softech</span> Dashboard</p>
         </div>
     );
 };
