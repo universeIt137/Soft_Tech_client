ae984595ae0bbcd378ea89d80a89981e13b07a9c
ashikur <ashikur1602@gmail.com>
2024-12-21 12:22:56 +0600

    team dropdown completed

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,34 +1,30 @@
-import React from 'react';
-import Sidebar from '../components/adminSide/Sidebar';
-import AdminNavbar from '../components/adminSide/AdminNavbar';
-import { Outlet } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
-import DashboardPage from '../pages/adminSide/dashboard/DashboardPage';
+import React from "react";
+import Sidebar from "../components/adminSide/Sidebar";
+import AdminNavbar from "../components/adminSide/AdminNavbar";
+import { Outlet } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const DashboardLayout = () => {
-
-    
-    return (
-        <div className="flex">
-            <Helmet>
-                <title>Dashboard</title>
-            </Helmet>
-            {/* Sidebar */}
-            <Sidebar></Sidebar>
-
-            {/* Main Content */}
-            <div className="flex-1 flex flex-col">
-                {/* Top Navbar */}
-                <AdminNavbar></AdminNavbar>
-
-                {/* Main Dashboard Content */}
-                <main className="flex-1 overflow-auto m-5">
-                   
-                    <Outlet></Outlet>
-                </main>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex min-h-screen bg-primary">
+      <Helmet>
+        <title>Dashboard</title>
+      </Helmet>
+      {/* Sidebar */}
+      <div className="bg-primary  min-h-screen">
+        <Sidebar />
+      </div>
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col bg-gray-50">
+        {/* Top Navbar */}
+        <AdminNavbar />
+        {/* Main Dashboard Content */}
+        <main className="flex-1 overflow-auto m-5">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
